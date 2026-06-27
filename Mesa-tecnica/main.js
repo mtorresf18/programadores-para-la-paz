8ca57cc8d33603186c9af8450befa2f000a577c0
Melissa Torres Forero <mtorresf@unicartagena.edu.co>
2026-06-27 15:43:00 +0000

    Actividad semana 4

---
+++ Mesa-tecnica/main.js
@@ -0,0 +1,10 @@
+console.log("Hola Mundo")
+
+const multiplicando = 9
+const multiplicador = 2
+
+console.log(multiplicando*multiplicador)
+
+console.error("mi error")
+console.warn()
+ // console error rojo, console warn amarillo, 
